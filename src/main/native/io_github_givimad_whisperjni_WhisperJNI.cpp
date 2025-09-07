6533cec1318ec837d90d0dbbe467263a63812af1
İsmail Samir USTA <Noxob@users.noreply.github.com>
2025-09-08 00:51:20 +0300

    Fetch whisper.cpp sources when submodule absent

--- src/main/native/io_github_givimad_whisperjni_WhisperJNI.cpp
+++ src/main/native/io_github_givimad_whisperjni_WhisperJNI.cpp
@@ -13,7 +13,7 @@ static JavaVM *jvmRef = nullptr;
 static void whisper_log_proxy(enum ggml_log_level level, const char * text, void * user_data) {
     if(jvmRef) {
         JNIEnv *env;
-        if (jvmRef->AttachCurrentThread(&env, nullptr) != JNI_OK) {
+        if (jvmRef->AttachCurrentThread(reinterpret_cast<void **>(&env), nullptr) != JNI_OK) {
           return;
         }
         jclass whisperJNIClass = env->FindClass("io/github/givimad/whisperjni/WhisperJNI");
